3a3d788f6ea0f686370f242d24e37642c4b487af
Aaron Godin <godinaaweb@gmail.com>
2022-01-19 13:24:48 -0600

    Update the readme with validation examples, remove example files, and update the workflow

--- src/error.cr
+++ src/error.cr
@@ -1,7 +1,8 @@
 module JSONSchema
   # Exception thrown at compile-time for any invalid schema. A schema may be invalid if the file cannot be read,
   # the JSON string in the file is not valid JSON (cannot be parsed), or if the resulting JSON has invalid 
-  # definitions for JSON Schema. Compile-time checks will throw this error if your schema cannot be turned into valid Crystal code.
+  # definitions for JSON Schema. Compile-time checks will throw this error if your schema cannot be turned into valid Crystal code,
+  # or to protect your application from runtime errors related to a schema that would not make logical sense.
   class InvalidSchemaError < Exception
     def initialize(message : String?, cause : Exception?, filename : String?)
       full_msg = String.build do |str|
